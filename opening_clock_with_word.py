fc34485640b029ad36a50097c454093f3e8b8ec4
Jason Luu <jasonluu121@gmail.com>
2022-07-01 23:11:17 -0400

    Update opening_clock_with_word.py
    Added Win10Toast to notify user after a ## minutes has passed. Separated some of the create_window function code to a separate function named update_window and that function is about updating the clock and elapsed time. Added show_notification function uses Win10Toast to notify user after (currently 30 mins passed)

--- opening_clock_with_word.py
+++ opening_clock_with_word.py
@@ -1,23 +1,26 @@
-# Reference for using WMI to get a list currently running services (Method 2 using OS)
+# Reference for using WMI to get a list currently running services
 #https://www.geeksforgeeks.org/python-get-list-of-running-processes/
-# Reference for getting elapsed time (Using DateTime module)
+# Reference for getting elapsed time
 # https://www.geeksforgeeks.org/how-to-measure-elapsed-time-in-python/
 # Reference for the GUI
-# PySimpleGUI.readthedocs.io
-# Reference for https://docs.python.org/3/library/datetime.html#timedelta-objects
-
+# PySimpleGUI
 from tabnanny import check
-from time import strftime
+from time import strftime, time
 import wmi
 import PySimpleGUI as sg
 from datetime import datetime
 import sys
 import os
+from win10toast import ToastNotifier
+from playsound import playsound
+
 word_process_name = 'WINWORD.EXE'
+time_to_show_notification = 30
 
 def main():
     start_time = check_word()
-    create_window(start_time)
+    toaster = ToastNotifier()
+    create_window(start_time,toaster)
 
 # Checks for Microsoft Word's status
 # If Word is running quit the loop
@@ -33,22 +36,13 @@ def check_word():
             print("Microsoft Word is running")
             break
     start_time_word = datetime.now()
-    # while True:
-    #     f = wmi.WMI()
-    #     for process in f.Win32_Process():
-    #         # Check if Microsoft Word has started
-    #         if process.Name == word_process_name:
-    #             print("Microsoft Word is running")
-    #             is_word_running = True
-
-    #     if is_word_running:
-    #         start_time_word = datetime.now()
-    #         break
+    
     return start_time_word
 
 # Makes a window using PySimpleGUI that shows the clock and elapsed time since Word was started
 # The window is Always On Top and Asynchronous (no user input needed)
-def create_window(start_time):
+def create_window(start_time,toaster):
+    
     #sg.theme('#211AE2')   # Add a little color to your windows
     # All the stuff inside your window. This is the PSG magic code compactor...
     layout = [  [sg.Text('Time:', font='Arial, 17', text_color='#3A3ABC', background_color = '#A0AE73',key='-OUTPUT_TIME-')],
@@ -57,21 +51,18 @@ def create_window(start_time):
             ]
 
     # Create the Window
-    window = sg.Window('Current Time', layout, keep_on_top = True, grab_anywhere=True,location=(500,50),background_color = '#A0AE73')
+    window = sg.Window('Current Time', layout, keep_on_top = True, grab_anywhere=True,location=(550,50),background_color = '#A0AE73')
     # Event Loop to process "events"
     while True:             
         event, values = window.read(timeout=100)
         if event in (sg.WIN_CLOSED, 'Cancel'):
            break
 
+        update_time(window, start_time, toaster)
+
         check_word_stop()
 
-        current_time = datetime.now()
-        cur_time = combine_strings('Time:', str(current_time.strftime("%H:%M")))
-        window['-OUTPUT_TIME-'].update(cur_time)
-        elapsed_time = str(current_time-start_time).split(':')
-        elap_time = combine_strings(elapsed_time[0] + ':' + elapsed_time[1], 'has passed (in H:M)')
-        window['-ELAPSED_TIME-'].update(elap_time)
+        
 
     window.close()
 
@@ -85,6 +76,27 @@ def check_word_stop():
 
     if word_process_name not in output:
         sys.exit()
+
+# Continously updates time to the popup window
+# Update elapsed time as well
+def update_time(window, start_time,toaster):
+    current_time = datetime.now()
+    cur_time = combine_strings('Time:', str(current_time.strftime("%H:%M")))
+    window['-OUTPUT_TIME-'].update(cur_time)
+    elapsed_time = str(current_time-start_time).split(':')
+    print(elapsed_time)
+    elap_time = combine_strings(elapsed_time[0] + ':' + elapsed_time[1], 'has passed (in H:M)')
+    show_notification(*(elapsed_time[1],elapsed_time[2]), toaster)
+    window['-ELAPSED_TIME-'].update(elap_time)
+
+# Uses Win10Toast to show a notification with ## minutes has passed
+# I put it to 30 minutes
+def show_notification(time_mins, time_ms, toaster):
+    time_mins = int(time_mins)
+    time_ms = float(time_ms)
+    if time_mins >= time_to_show_notification and time_mins % time_to_show_notification == 0:
+        if  1>= time_ms >= 0:
+            toaster.show_toast("Notification","{} minutes has passed".format(time_mins),duration=5,threaded=True)
 
 
 if __name__ == '__main__':
